1321035fcad14262cd4bceaca54f3aa9e4b36179
Michelle Wong <mwong@mozilla.com>
2019-07-16 10:34:42 -0700

    created constants file for types/names and created new general music page functions (untested)

---
+++ src/constants.js
@@ -0,0 +1,10 @@
+export const types = {
+    ALBUM_ART: 'albumArt'
+};
+
+export const names = {
+    ART_ASPECT_RATIO: 'artAspectRatio',
+    ART_AREA: 'artAreaSize',
+    ART_HORIZONTALLY_CENTERED: 'artHorizontallyCentered',
+    HAS_IMG_EXT: 'hasImgExt'
+};
--- src/trainees.js
+++ src/trainees.js
@@ -1,14 +1,46 @@
-import { ruleset, rule, dom, type, score, out } from 'fathom-web';
+'use strict';
+
+import { ruleset, rule, dom, type, score, out} from 'fathom-web';
 import { euclidean } from 'fathom-web/clusters';
-import { ancestors, isVisible, min } from 'fathom-web/utilsForFrontend';
+import { ancestors, isVisible, min , page} from 'fathom-web/utilsForFrontend';
+import { types, names } from './constants';
 
-const VIEWPORT_SIZE = { width: 1100, height: 900 };
+const VIEWPORT_DIMENS = { width: 1100, height: 900 };
+const VIEWPORT_SIZE = VIEWPORT_DIMENS.width * VIEWPORT_DIMENS.height;
+const IMAGE_EXTENSIONS = ['jpg', 'png', 'gif', 'webp', 'tiff', 'psd', 'raw', 'bmp', 'heif', 'indd', 'svg', 'ai', 'eps', 'pdf']
+const MUSIC_KEYWORDS = ['album', 'song', 'music', 'artist', 'genre']
 
-const ALBUM_ART = 'albumArt';
-const ART_ASPECT_RATIO = 'artAspectRatio';
+/* General Page Rules */
 
-const byAspectRatio = fnode => {
-    const element = fnode.element;
+// Checks for keyword in page URL
+function hasMusicKeywordInURL() {
+    return MUSIC_KEYWORDS.some(it => document.URL.includes(it));
+}
+
+// Checks for keyword in page title   
+function hasMusicKeywordInTitle() {
+    return MUSIC_KEYWORDS.some(it => document.title.includes(it));
+}
+
+// Checks for keyword in search bar placeholder text 
+// @param element - input element
+function hasMusicSearchBar(element) {
+    return MUSIC_KEYWORDS.some(it => element.placeholder.includes(it));
+}
+
+// Return total size of all images to total size (width * height) of text boxes
+function totalImageToTextSizeRatio(element) { 
+    // TODO
+}
+
+// Return count of square images compared to all images on screen
+function ratioOfSquareToTotalImgs() {
+    // TODO
+}
+
+/* Album art Rules */
+
+function byAspectRatio(element) {
     if (!element.offsetWidth || !element.offsetHeight) return 0;
 
     const bigger = Math.max(element.offsetWidth, element.offsetHeight);
@@ -18,8 +50,7 @@ const byAspectRatio = fnode => {
     return smaller / bigger;
 };
 
-const divIsImage = fnode => {
-    const element = fnode.element;
+function divIsImage(element) {
     const id = element.id.toLowerCase();
     const classes = [...element.classList].map(it => it.toLowerCase());
 
@@ -28,42 +59,123 @@ const divIsImage = fnode => {
     return allClassifiers.some(it => it.includes("image") || it.includes("img") || it.includes("art") || it.includes("album"));
 };
 
+function hasImageAttribute(element) {
+    const attributes = [...element.attributes];
+
+    for (let ext of IMAGE_EXTENSIONS) {
+        for (let attr of attributes) {
+            if (attr.value.includes('.' + ext)) return true;
+        }
+    }
+    return false;
+}
+
+function getHorizontallyCentered(element) {
+    const bodyRect = document.body.getBoundingClientRect()
+    const elementRect = element.getBoundingClientRect()
+
+    // const left = elementRect.left - bodyRect.left
+    // const right = bodyRect.right - elementRect.right
+
+    // return Math.min(left, right) / Math.max(left, right)
+
+    const center = (bodyRect.right - bodyRect.left) / 2;
+    const elementCenter = elementRect.x + ((elementRect.right - elementRect.left) / 2);
+
+    const bigger = Math.max(center, elementCenter);
+    const smaller = Math.min(center, elementCenter);
+
+    return 1 - ((bigger - smaller) / center);
+}
+
+function sizeToViewport(element) {
+    return (element.offsetHeight * element.offsetWidth) / VIEWPORT_SIZE;
+}
+
+/* Helper functions */ 
+// TODO: move to a different file?
+
+// Return array of all descendent nodes, including root element
+// @param element - root element to flatten
+function flattenNodes(element) {
+    let descendents = [];
+    let next = [element];
+    let curr;
+    while (next) { 
+        curr = next.shift();
+        all.push(curr);
+        next.concat(curr.children);
+    }
+    return descendents;
+}
+
 function makeRuleset() {
     return ruleset([
         // Image
         // Aspect ratio
         // Size
-        // Proximity to other elements TODO
+        // Proximity to other elements TODO clustering (?)
         // Horizontally centered
         // id or class with art/album
 
         // All visible images
-        rule(dom('img').when(isVisible), type(ALBUM_ART)),
-        rule(dom('div').when(divIsImage), type(ALBUM_ART)),
+        rule(dom('img').when(isVisible), type(types.ALBUM_ART)),
+        rule(dom('div').when(fnode => divIsImage(fnode.element)), type(types.ALBUM_ART)),
+
+        // Get largeness compared to viewport
+        rule(
+            type(types.ALBUM_ART),
+            score(fnode => sizeToViewport(fnode.element)),
+            { name: names.ART_AREA }
+        ),
         // Aspect ratios of images
         rule(
-            type(ALBUM_ART),
-            score(byAspectRatio),
-            { name: ART_ASPECT_RATIO }
+            type(types.ALBUM_ART),
+            score(fnode => byAspectRatio(fnode.element)),
+            { name: names.ART_ASPECT_RATIO }
+        ),
+        // get horizontally centered score
+        rule(
+            type(types.ALBUM_ART),
+            score(fnode => getHorizontallyCentered(fnode.element)),
+            { name: names.ART_HORIZONTALLY_CENTERED }
+        ),
+
+        rule(
+            type(types.ALBUM_ART),
+            score(fnode => hasImageAttribute(fnode.element)),
+            { name: names.HAS_IMG_EXT }
         ),
 
         // Output
-        rule(type(ALBUM_ART).max(), out(ALBUM_ART))
+        rule(type(types.ALBUM_ART).max(), out(types.ALBUM_ART))
     ]);
 };
 
 const config = new Map();
 
 config.set(
-    ALBUM_ART,
+    types.ALBUM_ART,
     {
         coeffs: new Map([
-            [ART_ASPECT_RATIO, 1]
+            [names.ART_ASPECT_RATIO, 1],
+            [names.ART_AREA, 1],
+            [names.ART_HORIZONTALLY_CENTERED, 1],
+            [names.HAS_IMG_EXT, 1]
         ]),
-        viewportSize: VIEWPORT_SIZE,
-        vectorType: ALBUM_ART,
+        viewportSize: VIEWPORT_DIMENS,
+        vectorType: types.ALBUM_ART,
         rulesetMaker: makeRuleset
     }
 );
 
 export default config;
+
+
+// module.exports = {
+//     byAspectRatio: byAspectRatio,
+//     divIsImage: divIsImage,
+//     getHorizontallyCentered: getHorizontallyCentered,
+//     sizeToViewport: sizeToViewport,
+//     hasImageAttribute: hasImageAttribute
+// }
